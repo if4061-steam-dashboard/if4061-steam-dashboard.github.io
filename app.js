8b2b006d1ce4ea5bb7d54f8e4c34d1cdf3d000b6
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2023-04-25 21:18:59 +0800

    feat: add gradient to bar

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ function makeBarChart(genre, startTimeAttribute, endTimeAttribute) {
 
     graph.selectAll("*").remove();
 
+    const defs = graph.append("defs");
+    const bwGradient = defs.append("linearGradient")
+        .attr("id", "bwGradient")
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "0%");
+    
+    bwGradient.append("stop")
+        .attr("offset", "0%")
+        .attr("style", "stop-color:rgb(0, 0, 0); stop-opacity:1");
+
+    bwGradient.append("stop")
+        .attr("offset", "100%")
+        .attr("style", "stop-color:rgb(255, 255, 255); stop-opacity:1");
+
     const barWidth = config.barChart.barWidth;
     const barHeight = config.barChart.barHeight;
     const barSpace = config.barChart.barSpace;
@@ -26,7 +42,6 @@ function makeBarChart(genre, startTimeAttribute, endTimeAttribute) {
     const aggregated_dataset_path = config.dataset.aggregatedPath;
     d3.csv(aggregated_dataset_path, (d, i) => {
         const genre = d[genreAttribute];
-        // const playerBase = parseFloat(d[startTimeAttribute]);
 
         let playerBase = 0.0;
         let pick = false;
@@ -62,12 +77,20 @@ function makeBarChart(genre, startTimeAttribute, endTimeAttribute) {
 
         bar.append("rect")
             .attr("width", playerBase * barWidth / maxPlayerBase)
-            .attr("height", barHeight);
+            .attr("height", barHeight)
+            .attr("fill", "url(#bwGradient)");
 
         bars.push(bar);
 
-        bars.sort((bar1, bar2) => bar2.attr("playerBase") - bar1.attr("playerBase"))
-            .filter((_, barIndex) => barIndex < barMaxCount)
+        bars.sort((bar1, bar2) => bar2.attr("playerBase") - bar1.attr("playerBase"));
+
+        if (bars.length == barMaxCount + 1) {
+            const bar = bars.pop();
+            bar.remove();
+        }
+
+
+        bars
             .forEach((bar, barIndex) => {
                 bar.attr("transform", `translate(0, ${barIndex * (barHeight + barSpace)})`);
             });
